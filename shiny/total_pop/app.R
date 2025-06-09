928b260438e1a61d5cc719e5e3b43a83695ed6a7
Eric Mai <ericvmai@gmail.com>
2025-06-09 12:32:49 -0400

    Update app.R

--- shiny/total_pop/app.R
+++ shiny/total_pop/app.R
@@ -70,7 +70,7 @@ ui <- page_fillable(
         margin: 0 auto; 
         border: 2px solid #011E41; 
         border-radius: 5px; 
-        padding: 45px;
+        padding: 15px;
       }
       
       /* Header styles */
